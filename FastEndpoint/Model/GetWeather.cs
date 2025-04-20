ed182259d2eaa3382d4dff301334e95fc3436e26
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2025-04-20 18:33:10 +0330

    Adding model dtos

---
+++ FastEndpoint/Model/GetWeather.cs
@@ -0,0 +1,9 @@
+﻿namespace FastEndpoint.Model
+{
+    public class GetWeather
+    {
+        public string CityName { get; set; }
+        public int CityId { get; set; }
+        public int Degree { get; set; }
+    }
+}
---
+++ FastEndpoint/Model/SaveWeather.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastEndpoint.Model
+{
+    public class SaveWeather
+    {
+        public string CityName { get; set; }
+        public int CityId { get; set; }
+        public int Degree { get; set; }
+    }
+    public class SaveWeatherValidator : Validator<SaveWeather>
+    {
+        public void CreateWeatherValidator()
+        {
+            RuleFor(q => q.CityName).NotEmpty()
+                .WithMessage("City name has not defined");
+            RuleFor(q => q.CityId).NotEmpty()
+                .WithMessage("City id has not defined");
+            RuleFor(q => q.Degree).NotEmpty()
+                .WithMessage("weather degree has not defined");
+        }
+    }
+}
